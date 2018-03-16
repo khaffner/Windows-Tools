ba85c818e41984572d4d202a0c6eefa9accb6eaf
Kevin Haffner <khaffner@gmail.com>
2018-03-16 20:03:41 +0100

    Erroractionpreference and installations
    Erroractionpreference = 'Stop', automatic install of VLC, Chrome, VSCode and Github Desktop

--- ConfigureMyComputer.ps1
+++ ConfigureMyComputer.ps1
@@ -4,20 +4,38 @@ $IsAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).gr
 $LARUserName = 'A'
 $STDUserNAme = 'Kevin'
 
-if($IsAdmin) {
-    if($env:USERNAME -eq $STDUserNAme) {
+$ErrorActionPreference = 'Stop'
+
+if ($IsAdmin) {
+    if ($env:USERNAME -eq $STDUserNAme) {
         #My user is admin :(
         #Install some things that need administrative privileges.
 
+        $VlCInstaller = "$env:TEMP\VLC.exe"
+        Invoke-WebRequest -Uri 'https://ftp.acc.umu.se/mirror/videolan.org/vlc/3.0.1/win64/vlc-3.0.1-win64.exe' -OutFile $VlCInstaller
+        Start-Process -FilePath $VlCInstaller -ArgumentList '/S'
+
+        $ChromeInstaller = "$env:TEMP\Chrome.msi"
+        Invoke-WebRequest -Uri 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi' -OutFile $ChromeInstaller
+        Start-Process -FilePath msiexec -ArgumentList "/i $ChromeInstaller /qn" -Wait
+
+        $VScodeInstaller = "$env:TEMP\vscode.exe"
+        Invoke-WebRequest -Uri "https://go.microsoft.com/fwlink/?Linkid=852157" -OutFile $VScodeInstaller
+        Start-Process -FilePath $VScodeInstaller -ArgumentList "/verysilent /suppressmsgboxes"
+
+        $GithubDesktopInstaller = "$env:TEMP\GithubDesktop.exe"
+        Invoke-WebRequest -Uri "https://central.github.com/deployments/desktop/desktop/latest/win32" -OutFile $VScodeInstaller
+        Start-Process -FilePath $GithubDesktopInstaller
+
         #Creating the LAR user if it does not exist.
         $LARUser = Get-LocalUser -Name $LARUserName
-        if(!($LARUser)) {
+        if (!($LARUser)) {
             #LARUser does not exist
             $LARUserPassword = Read-Host -Prompt 'Enter the password for the LAR User "A"' -AsSecureString
             New-LocalUser -Name $LARUserName -FullName 'LAR User' -PasswordNeverExpires -Password $LARUserPassword
         }
         else {
-            if((Get-LocalGroupMember -Name 'Administrators').Name -notcontains "$env:COMPUTERNAME\$($LARUser.Name)") {
+            if ((Get-LocalGroupMember -Name 'Administrators').Name -notcontains "$env:COMPUTERNAME\$($LARUser.Name)") {
                 #A is not admin
                 Add-LocalGroupMember -Group 'Administrators' -Member (Get-LocalUser -Name $LARUserName)
             }
